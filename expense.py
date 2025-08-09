e9c58cebe01cce876c523c8efc7ccd2e145ddb00
arash fazaeli <a.fazaeli1980@gmail.com>
2025-08-09 16:09:49 +0330

    final code

--- expense.py
+++ expense.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException,status
 
 app =  FastAPI()
 
@@ -10,7 +10,7 @@ expenses_db = [
 ]
 
 
-@app.post("/create-item")
+@app.post("/create-item",status_code=status.HTTP_201_CREATED)
 async def create_item(desc: str, amount:float):
     last_id = expenses_db[-1]["id"] 
     last_id = int(last_id + 1)
@@ -22,12 +22,12 @@ async def create_item(desc: str, amount:float):
     expenses_db.append(new_expense)
     return new_expense
 
-@app.get("/cost-list")
+@app.get("/cost-list", status_code=status.HTTP_200_OK)
 async def cost_list():
     return expenses_db
 
 
-@app.put("/cost/{cost_id}")
+@app.put("/cost/{cost_id}", status_code=status.HTTP_200_OK)
 async def cost_update(cost_id: int,desc: str, amount:float):
     for item in expenses_db:
         if item["id"] == cost_id:
@@ -38,7 +38,7 @@ async def cost_update(cost_id: int,desc: str, amount:float):
     raise HTTPException(status_code=404,detail="id not found")
 
   
-@app.delete("/cost/{cost_id}")
+@app.delete("/cost/{cost_id}", status_code=status.HTTP_200_OK)
 async def cost_to_delete(cost_id: int):
     for i in range(len(expenses_db)):
         if expenses_db[i]["id"] == cost_id:
@@ -48,7 +48,7 @@ async def cost_to_delete(cost_id: int):
     raise HTTPException(status_code=404, detail="id not found")
 
 
-@app.get("/all-items-cost")
+@app.get("/all-items-cost", status_code=status.HTTP_200_OK)
 async def all_items_cost():
     total = sum(item["amount"] for item in expenses_db)
     return {"total_cost": total}
